5c1ba58fe2512e4bb2b5c7bb022f93529b6d9028
Shijin Zhang <1364226740@qq.com>
2023-11-28 21:58:47 +0800

    Added Auth Middleware

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authenticate from './authenticate';
 import moviesRouter from './api/movies';
 import cors from 'cors';
 import usersRouter from './api/users';
@@ -14,7 +15,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', authenticate,  moviesRouter);
 app.use(defaultErrHandler);
 
 app.listen(port, () => {
