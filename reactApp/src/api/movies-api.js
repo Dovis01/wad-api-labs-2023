42037a385cd73139bb2f749e972e2067a92255cb
Shijin Zhang <1364226740@qq.com>
2023-12-05 02:01:34 +0800

    Topic 12 - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,13 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=3949a8e40149fe7ea222fce59c7d0f73&language=en-US&include_adult=false&page=1`
+    const response = await fetch(
+        'http://localhost:8080/api/movies', {
+            headers: {
+                'Authorization': window.localStorage.getItem('token')
+            }
+        }
     )
-    return response.json()
-  };
+    return response.json();
+};
 
 export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
